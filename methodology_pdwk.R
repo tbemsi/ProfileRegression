4773cad596baccf854b562e789b5ce23473aa4a8
pauldwkirk <paul.kirk@mrc-bsu.cam.ac.uk>
2021-03-11 22:33:33 +0000

    Updated methodology_pdwk.R

--- methodology_pdwk.R
+++ methodology_pdwk.R
@@ -25,6 +25,15 @@ indices <- sample(seq_len(nrow(inputs$inputData)), size = sample_size)
 train <- inputs$inputData[indices, ]
 test <- inputs$inputData[-indices, ]
 
+# To make visualisation easier, let's reorder by cluster:
+train <- train[sort(train$group, index.return = T)$ix,]
+test  <-  test[sort( test$group, index.return = T)$ix,]
+
+#To accelerate plotting, only plot 500 rows from each:
+pheatmap::pheatmap(train[sort(sample(nrow(train), 500)),2:ncol(train)], cluster_rows = F, cluster_cols = F)
+pheatmap::pheatmap(test[sort(sample(nrow(test), 500)),2:ncol(test)], cluster_rows = F, cluster_cols = F)
+#In both cases, the clustering structure is clear.
+
 
 ### transform each subsampled dataset
 data_transformation <- function(inputs){
@@ -47,6 +56,42 @@ data_transformation <- function(inputs){
 
 new.train <- data_transformation(train)
 new.test <- data_transformation(test)
+
+print(new.train$covNames)
+#[1] "group"      "Variable2"  "Variable3"  "Variable4"  "Variable5"  "Variable6"  "Variable7" 
+#[8] "Variable8"  "Variable9"  "Variable10" "Variable11" "Variable12" "Variable13" "Variable14"
+#[15] "Variable15" "Variable16" "Variable17" "Variable18" "Variable19" "Variable20" "Variable21"
+#[22] "Variable22" "Variable23" "Variable24" "Variable25" "Variable26" "Variable27"
+#It looks like "group" should not be included in this, and that "Variable1" might be missing.
+
+print(names(new.train$inputData))
+#[1] "group"      "Variable2"  "Variable3"  "Variable4"  "Variable5"  "Variable6"  "Variable7" 
+#[8] "Variable8"  "Variable9"  "Variable10" "Variable11" "Variable12" "Variable13" "Variable14"
+#[15] "Variable15" "Variable16" "Variable17" "Variable18" "Variable19" "Variable20" "Variable21"
+#[22] "Variable22" "Variable23" "Variable24" "Variable25" "Variable26" "Variable27" NA          
+#[29] "Outcome"  
+#There is a column ( #28) with the name "NA"
+
+#New version of the above to try to fix this:
+
+### transform each subsampled dataset
+data_transformation <- function(inputs){
+  ready_data <- vector(mode = "list")
+  rownames(inputs) <- seq(nrow(inputs))
+  ready_data$inputData <- inputs
+  #covnames <- c("group", rep(0, 26))
+  covnames <- names(inputs)[2:(length(names(inputs)) - 1)]
+  ready_data$covNames <- covnames
+  ready_data$inputData$Outcome <- inputs$Outcome
+  ready_data$xModel <- "Discrete"
+  ready_data$yModel <- "Bernoulli"
+  ready_data$nCovariates <- 26 
+  ready_data
+}
+
+new.train <- data_transformation(train)
+new.test <- data_transformation(test)
+
 
 #Run profile regression on both subsampled datasets
 runInfoObj.train   <- profRegr(yModel=new.train$yModel, 
@@ -72,6 +117,9 @@ myPSM.train <- makePSM(zMatrix)
 set.seed(seedy.seed)
 myClustersReduced.train <- maxpear(myPSM.train)$cl
 train$Clusters <- myClustersReduced.train
+
+#How good is the clustering:
+print(adjustedRandIndex(train$Clusters, new.train$inputData$group))
 
 #### Maximum likelihood model
 
@@ -131,50 +179,101 @@ number.clusters <- length(unique(myClustersReduced))
 
 test$Clusters <- myClustersReduced
 
+#How good is the clustering:
+print(adjustedRandIndex(test$Clusters, new.test$inputData$group))
+
+sampleInds <- sort(sample(nrow(new.test$inputData), 500))
+#Check output:
+pheatmap::pheatmap(myPSM.test[sampleInds,sampleInds], cluster_rows = F, cluster_cols = F)
+pheatmap::pheatmap(new.test$inputData[sampleInds,2:(ncol(new.test$inputData)-1)], cluster_rows = F, cluster_cols = F)
+
 
 ########----------------------------------------------
 
 theta.train <- compute_theta(train)
 theta.test <- compute_theta(test)
-observation.train = as.numeric(train[1, ][2:27])
-observation.test = as.numeric(test[1, ][2:27])
-posterior_prob(observation.train, train, theta.train)
-posterior_prob(observation.test, test, theta.test)
 
+#Let's check the above.
+#What are the cluster-specific parameters?
 
-#### take what has been trained on the training set, and apply to the testing set
-sth.test <- rep(0, nrow(test))
-for (i in 1:nrow(train)){
-  observation <- as.numeric(test[i, ][2:27])
-  sth.test[i] <- posterior_prob(observation, train, theta.train)
+mixtureWeightsTrain <- vector(mode = "numeric", length = length(unique(train$Clusters))) 
+clusterParamsTrain  <- matrix(nrow = length(unique(train$Clusters)), ncol = ncol(new.train$inputData) - 2)
+
+counter <- 1
+for(k in unique(train$Clusters))
+{
+  currentClusterData <- new.train$inputData[train$Clusters == k, (2:(ncol(new.train$inputData)-1))]
+  
+  mixtureWeightsTrain[counter] <- nrow(currentClusterData)/nrow(new.train$inputData)
+  clusterParamsTrain[counter,] <- colSums(currentClusterData)/nrow(currentClusterData)
+  
+  counter <- counter + 1
 }
-new.test <- test[, c(1:28)]
-new.test$Clusters <- sth.test
 
-final.df.one <- rbind(train, new.test)
+print(max(abs(clusterParamsTrain -  theta.train)))
+# Should be a very small number, which it is!
 
+posteriorProbs_predictingTest <- matrix(nrow = nrow(new.test$inputData), ncol = length(unique(train$Clusters)))
 
-sth.train <- rep(0, nrow(train))
-for (i in 1:nrow(test)){
-  observation <- as.numeric(train[i, ][2:27])
-  sth.train[i] <- posterior_prob(observation, test, theta.test)
+for(i in 1:nrow(posteriorProbs_predictingTest))
+{
+  currentDataForPrediction          <- new.test$inputData[i,2:(ncol(new.test$inputData)-1)]
+  unnormalisedProbs                 <- clusterParamsTrain[,as.logical(unlist(currentDataForPrediction)), drop = F]
+  unnormalisedProbs                 <- exp(log(mixtureWeightsTrain) + rowSums(log(unnormalisedProbs)))
+  posteriorProbs_predictingTest[i,] <- unnormalisedProbs/sum(unnormalisedProbs)
 }
-new.train <- train[, c(1:28)]
-new.train$Clusters <- sth.train
 
-final.df.two <- rbind(test, new.train)
+predictedClustersForTest <- apply(posteriorProbs_predictingTest, 1, which.max)
 
 
-ARI <- adjustedRandIndex(final.df.one, final.df.two)
+#Let's have a look at the predicted clusters: looks okay!
+pheatmap::pheatmap(new.test$inputData[sort(predictedClustersForTest, index.return = T)$ix, 2:(ncol(new.test$inputData)-1)], cluster_rows = F, cluster_cols = F)
 
+#Let's check the ARI with the truth:
+print(adjustedRandIndex(predictedClustersForTest, new.test$inputData$group))
 
-##### Produce matrix. Fit test using the training data, get the clustering allocations
-##### Matrix[i,j] = sum(Indicator(person is allocated in cluster i from premium, and cluster j from MLE))
-##### Maybe also use ARIs between different clusterings
+#Let's check the ARI with the clusters obtained from applying PReMiuM to test:
+print(adjustedRandIndex(predictedClustersForTest, test$Clusters ))
 
-#### New likelihood function which takes into account the outcome
-
-
+# observation.train = as.numeric(train[1, ][2:27])
+# observation.test = as.numeric(test[1, ][2:27])
+# posterior_prob(observation.train, train, theta.train)
+# posterior_prob(observation.test, test, theta.test)
+# 
+# 
+# #### take what has been trained on the training set, and apply to the testing set
+# sth.test <- rep(0, nrow(test))
+# for (i in 1:nrow(train)){
+#   observation <- as.numeric(test[i, ][2:27])
+#   sth.test[i] <- posterior_prob(observation, train, theta.train)
+# }
+# new.test <- test[, c(1:28)]
+# new.test$Clusters <- sth.test
+# 
+# final.df.one <- rbind(train, new.test)
+# 
+# 
+# sth.train <- rep(0, nrow(train))
+# for (i in 1:nrow(test)){
+#   observation <- as.numeric(train[i, ][2:27])
+#   sth.train[i] <- posterior_prob(observation, test, theta.test)
+# }
+# new.train <- train[, c(1:28)]
+# new.train$Clusters <- sth.train
+# 
+# final.df.two <- rbind(test, new.train)
+# 
+# 
+# ARI <- adjustedRandIndex(final.df.one, final.df.two)
+# 
+# 
+# ##### Produce matrix. Fit test using the training data, get the clustering allocations
+# ##### Matrix[i,j] = sum(Indicator(person is allocated in cluster i from premium, and cluster j from MLE))
+# ##### Maybe also use ARIs between different clusterings
+# 
+# #### New likelihood function which takes into account the outcome
+# 
+# 
 
 
 
